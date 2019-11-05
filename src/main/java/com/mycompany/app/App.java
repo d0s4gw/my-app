453f8694ad6a01ff8cb4e6f85a1cab514a06ada2
Michael Welsh <michael.j.welsh@gmail.com>
2019-11-04 21:28:33 -0800

    Initial commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,24 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	for (int i = 0; i < 100; i++) {
+		if (i % 15 == 0) {
+			System.out.println("fizzbuzz");
+		} else if (i % 5 == 0) {
+			System.out.println("buzz");
+		} else if (i % 3 == 0) {
+			System.out.println("fizz");
+		} else {
+			System.out.println(i);
+		}
+	}
+    }
+}
